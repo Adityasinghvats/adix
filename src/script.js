9f195942911d67c4556a9058211616b524cdcc4e
Adityasinghvats <131326798+Adityasinghvats@users.noreply.github.com>
2025-01-22 16:46:57 +0530

    Add interactive contact button and enhance index.html layout

---
+++ src/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const contact = document.getElementById('contact')
+
+    contact.addEventListener('click', () => {
+        alert("Contact Details!!")
+    })
+})
