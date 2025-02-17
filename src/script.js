b10bb46ee465c8afb53ac448d2ce1b6aa3074e83
Adityasinghvats <131326798+Adityasinghvats@users.noreply.github.com>
2025-02-18 02:14:32 +0530

    Refactor Tailwind CSS integration: replace configuration file with direct import and update dependencies; add clipboard functionality in script.js

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const contact = document.getElementById('contact');
     const menuBtn = document.getElementById('menu-btn');
     const menu = document.getElementById('menu');
+    const clibtn = document.getElementById('cli')
 
     contact.addEventListener('click', () => {
         window.location.href = 'https://github.com/Adityasinghvats/';
@@ -12,5 +13,20 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     menu.addEventListener('click', ()=>{
         menu.classList.toggle('hidden');
+    })
+
+    clibtn.addEventListener('click',async ()=>{
+        try {
+            await navigator.clipboard.writeText('npx adix');
+            const span = clibtn.querySelector('span');
+            const originalText = span.textContent;
+            span.textContent = 'Copied!';
+
+            setInterval(()=>{
+                span.textContent = originalText;
+            }, 2000)
+        } catch (error) {
+            console.error('Failed to copy text: ', err);
+        }
     })
 })
